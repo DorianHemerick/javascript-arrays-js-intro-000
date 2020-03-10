3c23d511350b426f67e6a56360c686b902668baa
Dorian Hemerick <dorianhemerick@gmail.com>
2020-03-10 21:04:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,3 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(chocolateBars, element){
   return [(element), ...chocolateBars];
 }
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
-  return [0] = [(element)]
-}
