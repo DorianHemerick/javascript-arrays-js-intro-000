4a682bf173034917a58f801d3d5122e777cad9f6
Dorian Hemerick <dorianhemerick@gmail.com>
2020-03-10 21:09:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(chocolateBars, element){
-  return [(element), ...chocolateBars];
-}
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
-  return chocolateBars.unshift(element, 1)
+  return chocolateBars.unshift(element);
 }
