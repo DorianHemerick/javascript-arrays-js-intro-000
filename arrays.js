88d88271a3d0462fa08d3f5d27ddcadecdba706a
Dorian Hemerick <dorianhemerick@gmail.com>
2020-03-10 21:03:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,5 @@ function addElementToBeginningOfArray(chocolateBars, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
-  return [(element)] = 0
+  return [(element)] = [0]
 }
